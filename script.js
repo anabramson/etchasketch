afc56d0043b3653f6fca43b39a7c9ea7cebd467e
anabramson <bramson.ana@gmail.com>
2025-03-30 13:36:41 +0200

    changed hover to use css

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.container');
     for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');
         
-        // Add hover effect
         square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'blue'; // Change to desired color
+            square.classList.add('hovered');
         });
 
         container.appendChild(square);
